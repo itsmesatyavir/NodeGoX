013988b0c13d02dcfcb6ac3dd29be689361a467a
itsmesatyavir <satyavirsatyarthi@gmail.com>
2025-03-08 16:47:56 +0530

    Update banner.js

--- banner.js
+++ banner.js
@@ -2,11 +2,13 @@ import chalk from 'chalk';
 
 export default function displayBanner() {
     console.log(chalk.cyan(`
-     F O R E S T A R M Y     
-                ${chalk.yellow('NODEGO AUTO - BOT')}                
-    📢  ${chalk.yellow('Telegram Channel: https://t.me/forestarmy)}`));
+     F O R E S T A R M Y
+                ${chalk.yellow('NODEGO AUTO - BOT')}
+    `));
 
     console.log(chalk.yellow('\n════════════════════════════════════════════════════════'));
     console.log(chalk.white(`Started at: ${new Date().toLocaleString()}`));
     console.log(chalk.yellow('════════════════════════════════════════════════════════\n'));
+
+    console.log(chalk.green('Join Telegram: ') + chalk.blue('https://t.me/forestarmy\n'));
 }
